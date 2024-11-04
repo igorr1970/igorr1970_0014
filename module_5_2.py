0bc7ebc37abc12207075d0a6bdd732e81107bc50
Игорь Рябухин <igorr1970@mail.ru>
2024-11-04 17:22:50 +0500

    Update module_5_2.py

--- module_5_2.py
+++ module_5_2.py
@@ -1,22 +1,19 @@
 class House:
     def __init__(self, name, number_of_floors):
-        self.name = name  # Название здания
-        self.number_of_floors = number_of_floors  # Количество этажей
+        self.name = name
+        self.number_of_floors = number_of_floors
 
     def __len__(self):
-        return self.number_of_floors  # Возвращаем количество этажей
+        return self.number_of_floors
 
     def __str__(self):
-        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"  # Форматируем строку для вывода
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
 
-# Пример выполнения кода
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 
-# __str__
-print(h1)  # Вывод: Название: ЖК Эльбрус, кол-во этажей: 10
-print(h2)  # Вывод: Название: ЖК Акация, кол-во этажей: 20
+print(h1)
+print(h2)
 
-# __len__
-print(len(h1))  # Вывод: 10
-print(len(h2))  # Вывод: 20
+print(len(h1))
+print(len(h2))
