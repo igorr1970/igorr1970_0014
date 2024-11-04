7fdacb679ab77adf056a1bb80d9745cc23e60f05
Игорь Рябухин <igorr1970@mail.ru>
2024-11-04 17:56:16 +0500

    Update module_5_3.py

--- module_5_3.py
+++ module_5_3.py
@@ -46,7 +46,6 @@ class House:
     def __iadd__(self, value):
         return self.__add__(value)
 
-# Пример использования класса House
 if __name__ == "__main__":
     h1 = House('ЖК Эльбрус', 10)
     h2 = House('ЖК Акация', 20)
@@ -54,20 +53,20 @@ if __name__ == "__main__":
     print(h1)
     print(h2)
 
-    print(h1 == h2)  # __eq__
+    print(h1 == h2)
 
-    h1 = h1 + 10  # __add__
+    h1 = h1 + 10
     print(h1)
     print(h1 == h2)
 
-    h1 += 10  # __iadd__
+    h1 += 10
     print(h1)
 
-    h2 = 10 + h2  # __radd__
+    h2 = 10 + h2
     print(h2)
 
-    print(h1 > h2)  # __gt__
-    print(h1 >= h2)  # __ge__
-    print(h1 < h2)  # __lt__
-    print(h1 <= h2)  # __le__
-    print(h1 != h2)  # __ne__
+    print(h1 > h2)
+    print(h1 >= h2)
+    print(h1 < h2)
+    print(h1 <= h2)
+    print(h1 != h2)
